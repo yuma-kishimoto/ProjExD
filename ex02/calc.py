b9854832148892fee4a5365c137c5d87e42a72c9
Yuma <c0a21161da@edu.teu.ac.jp>
2022-11-29 16:55:49 +0900

    ４限で要素をy追加した電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,25 +1,54 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+import math
 
 
 def button_click(event):
     btn = event.widget
     i = btn["text"]
-    if i == "=":
+    siki=entry.get()
+    if len(siki) >= 14:
+        entry.delete(0, tk.END)
+        entry.insert(0,"Error")
+    elif i == "=":
         siki=entry.get()
         res = eval(siki)
         entry.delete(0, tk.END)
         entry.insert(tk.END, res)
+    elif i == "C":
+        entry.delete(0, tk.END)
+    elif i == "√":
+        siki=entry.get()
+        res = math.sqrt(eval(siki))
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
+    elif i == "**2":
+        siki= eval(entry.get())
+        res = siki*siki
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
     else:
         entry.insert(tk.END, i)
-    
+
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("400x600")
 
 entry=tk.Entry(root, justify="right", width = 10, font = ("",40))
-entry.grid(row = 0, column=0, columnspan=3)
+entry.grid(row = 0, column=0, columnspan=4,sticky=tk.W+tk.E)
 
 r = 1
+c = 0
+kei = ["**2","√","C"]
+for Q in kei:
+    button = tk.Button(root, text = f"{Q}", width=4, height=2, font = ("", 30))
+    button.grid(row = r, column = c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c % 3 == 0:
+        r += 1
+        c = 0
+
+r = 2
 c = 0
 for i in range(9,-1,-1):
     button = tk.Button(root, text = f"{i}", width=4, height=2, font = ("", 30))
@@ -29,16 +58,27 @@ for i in range(9,-1,-1):
     if c % 3 == 0:
         r += 1
         c = 0
+button = tk.Button(root, text = f".", width=4, height=2, font = ("", 30))
+button.grid(row = 5, column = 1)
+button.bind("<1>",button_click)
+button = tk.Button(root, text = f"%", width=4, height=2, font = ("", 30))
+button.grid(row = 5, column = 2)
+button.bind("<1>",button_click)
+button = tk.Button(root, text = f"=", width=4, height=2, font = ("", 30), bg= "blue", fg = "white")
+button.grid(row = 5, column = 3)
+button.bind("<1>",button_click)
 
-ope = ["+", "-","*","/","="]
+n = 1
+m = 3
+ope = ["/", "*","-","+"]
 for w in ope:
     button = tk.Button(root, text = f"{w}", width=4, height=2, font = ("", 30))
-    button.grid(row = r, column = c)
+    button.grid(row = n, column = m)
     button.bind("<1>",button_click)
-    c += 1
-    if c % 3 == 0:
-        r += 1
-        c = 0
+    m += 1
+    if m % 3 == 1:
+        n += 1
+        m = 3
 
 
 root.mainloop()
